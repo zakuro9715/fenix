3d6274884b2ea3f94f6c9a4c060e205bb33bc34b
zakuro <z@kuro.red>
2020-09-12 20:48:06 +0900

    Print hello message

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 #![no_main]
 
 extern crate rlibc;
+mod vga_buffer;
+
 
 use core::panic::PanicInfo;
 
@@ -10,18 +12,8 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
+    vga_buffer::print_text("hello");
     loop {}
-
 }
