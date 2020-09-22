8195b907e84e6bdbc33978154ff784c049b712c0
zakuro <z@kuro.red>
2020-09-23 04:20:17 +0900

    Add first integration test

---
+++ tests/basic_boot.rs
@@ -0,0 +1,24 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(fenix::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+use fenix::println;
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    fenix::handle_panic(info)
+}
+
+#[test_case]
+fn test_println() {
+    println!("test");
+}
