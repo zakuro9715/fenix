ae8a57c5ba4d3a49bbaa9dba09d44ce872e5dd35
zakuro <z@kuro.red>
2020-09-23 02:51:25 +0900

    Improve output

--- src/main.rs
+++ src/main.rs
@@ -32,26 +32,55 @@ fn panic(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    serial_println!("\n[Failed]");
-    serial_println!("Error: {}\n", info);
+    serial_println!("\x1b[1G\x1b[1m\x1b[31m[Failed]\x1b[m"); // bold red -> Same as [OK]. See Testable
+    serial_println!();
+    serial_println!("Error: {}", info);
+    serial_println!();
+
     qemu::exit_qemu(qemu::QemuExitCode::Failed);
     loop {}
 }
 
 
-#[cfg(test)]
-fn run_tests(tests: &[&dyn Fn()]) {
-    serial_println!("Fenix test.\nRunning {} tests...", tests.len());
-    for test in tests {
-        test();
+trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        let name = core::any::type_name::<T>();
+        serial_print!("\t  {}", name); // -> __________name
+        if name.len() < 16 {
+            serial_print!("\t");
+        }
+        self();
+        serial_println!("\x1b[1G\x1b[1m\x1b[32m[OK]\x1b[m");  // bold green -> [ok]______name
     }
-    serial_println!("Success!");
+}
+
+#[cfg(test)]
+fn run_tests(tests: &[&dyn Testable]) {
+    serial_println!();
+    serial_println!("\x1b[1m\x1b[38;5;202mFenix test mode.\x1b[m"); // bold orange
+    serial_println!("Running {} tests...", tests.len());
+    serial_println!();
+
+    for test in tests {
+        test.run();
+    }
+
+    serial_println!();
+    serial_println!("\x1b[1m\x1b[32mSuccess!\x1b[m"); // bold green
+
     qemu::exit_qemu(qemu::QemuExitCode::Success);
 }
 
 #[test_case]
-fn test_example() {
-    assert_eq!(1, 1);
+fn test_ok() {
+    assert!(true);
 }
 
 #[cfg(feature="test_fail")]
